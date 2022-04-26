5c41e92d643c1ac6a7e67d87687ca25d9ff11897
Max Mäkipää <max.makipaa@gmail.com>
2022-04-26 14:05:37 +0300

    formi toimii tällä hetkellä. Valinnat ei tallennu, jos sivun refreshaa, eli backihommat ja formin ulkonäön muokkaus vielä puuttuu.

--- frontend/src/pages/_dashboard/slug/checklist/index.js
+++ frontend/src/pages/_dashboard/slug/checklist/index.js
@@ -1,40 +1,82 @@
-import React from "react";
-import Checkbox from "@material-ui/core/Checkbox";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import PageHeader from 'components/generic/PageHeader';
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import FormLabel from '@material-ui/core/FormLabel'
+import FormControl from '@material-ui/core/FormControl'
+import FormGroup from '@material-ui/core/FormGroup'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
 
-
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+    },
+    formControl: {
+        margin: theme.spacing(3),
+    },
+}))
 
 export default () => {
-    const [checked, setChecked] = React.useState(true);
-    const handleChange = (event) => {
-        setChecked(event.target.checked);
-    };
+    const classes = useStyles()
+
+    const [state, setState] = React.useState({
+        firstCB: false,
+        secondCB: false,
+        thirdCB: false,
+        fourthCB: false,
+        fifthCB: false,
+    })
+
+    const handleChange = event => {
+        setState({ ...state, [event.target.name]: event.target.checked })
+    }
+
+    const checklistObjects = [
+        {
+            name: 'first checkbox',
+            state: state[0],
+            label: 'Start searching for team',
+        },
+        {
+            name: 'second checkbox',
+            state: state[1],
+            label: 'Start coding with your team',
+        },
+        {
+            name: 'third checkbox',
+            state: state[2],
+            label: 'Submit your code',
+        },
+        {
+            name: 'fourth checkbox',
+            state: state[3],
+            label: 'Evaluate at least 5 other projects',
+        },
+        {
+            name: 'fifth checkbox',
+            state: state[4],
+            label: 'Congratulate yourself for a great hackathon success',
+        },
+    ]
 
     return (
-        <>
-            <PageHeader
-                heading="Checklist"
-                subheading="yassified"
-            />
-            <div style={{
-                display: "flex",
-                flexDirection: "column",
-            }}>
-                <FormControlLabel
-                    control={<Checkbox checked={checked} onChange={handleChange} />}
-                    label="Check me"
-
-                />
-                <FormControlLabel
-                    control={<Checkbox checked={checked} onChange={handleChange} />}
-                    label="jee"
-                />
-                <FormControlLabel
-                    control={<Checkbox checked={checked} onChange={handleChange} />}
-                    label="jeess"
-                />
-            </div>
-        </>
-    );
+        <div className={classes.root}>
+            <FormControl component="fieldset" className={classes.formControl}>
+                <FormLabel component="legend">Event checklist</FormLabel>
+                <FormGroup>
+                    {checklistObjects.map(value => (
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    checked={value.state}
+                                    onChange={handleChange}
+                                    name={value.name}
+                                />
+                            }
+                            label={value.label}
+                        />
+                    ))}
+                </FormGroup>
+            </FormControl>
+        </div>
+    )
 }
